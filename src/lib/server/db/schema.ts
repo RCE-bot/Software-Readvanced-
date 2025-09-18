d7189288c1327020d4bc10280a73eda75ca5b773
Airstriker <114228623+Airstriker123@users.noreply.github.com>
2025-09-19 09:19:50 +1000

    TS
    reverted to ts

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,6 +1,5 @@
 import { mysqlTable, serial, int } from 'drizzle-orm/mysql-core';
-
-export const user = mysqlTable('user', {
+mysqlTable('user', {
 	id: serial('id').primaryKey(),
 	age: int('age')
 });
