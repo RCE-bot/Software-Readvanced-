0e406fc6e543a6000e1af5487289773b4a27c398
Airstriker <114228623+Airstriker123@users.noreply.github.com>
2025-09-21 20:36:32 +1000

    Menu test example 1

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,5 +1,12 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
+// get other plugins:
+import Flip from "gsap/Flip";
+// or all tools are exported from the "all" file (excluding members-only plugins):
+import { gsap, ScrollTrigger, Draggable, MotionPathPlugin } from "gsap/all";
+// don't forget to register plugins
+gsap.registerPlugin(ScrollTrigger, Draggable, Flip, MotionPathPlugin);
+
 declare global {
 	namespace App {
 		// interface Error {}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+</script>
+
+<svelte:head>
+    <nav2>
+        <a href="#what-is-a-whale?" class="btn">What is a whale?</a>
+        <a href="#MoreInformation" class="btn">More information about whales</a>
+        <a href="#feedback" class="btn">Feedback</a>
+    </nav2>
+</svelte:head>
+
+<style>
+    :root
+    {
+        background: black;
+
+    }
+</style>
