6c750c2744440e66ca31c3dc7c65f7c5a0c3e3a1
Airstriker <114228623+Airstriker123@users.noreply.github.com>
2025-11-01 00:17:35 +1100

    delete temp files

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.h1c7rfh3ue"
+    "revision": "0.l3t91hqooko"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
